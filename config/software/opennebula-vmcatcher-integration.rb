ab38ce52d1b77fd0878520d711d91e1522b5c1fc
Michal Kimle <kimle.michal@gmail.com>
2016-03-20 14:22:35 +0100

    added vmcatcher configuration files

--- config/software/opennebula-vmcatcher-integration.rb
+++ config/software/opennebula-vmcatcher-integration.rb
@@ -6,5 +6,6 @@ build do
   mkdir "#{install_dir}/bin"
   mkdir "#{install_dir}/etc"
   copy "#{project.files_path}/opennebula-vmcatcher-integration-cronjob", "#{install_dir}/etc/opennebula-vmcatcher-integration-cronjob"
+  copy "#{project.files_path}/vmcatcher-env", "#{install_dir}/etc/vmcatcher-env"
   copy "#{project.files_path}/opennebula-vmcatcher-integration", "#{install_dir}/bin/opennebula-vmcatcher-integration"
 end
