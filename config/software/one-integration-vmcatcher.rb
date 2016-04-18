ffa4570547b8c5ba6207c75d4fa6b8c2543cf282
Michal Kimle <kimle.michal@gmail.com>
2016-04-18 11:44:37 +0200

    removed installation of vmcatcher configuration file

--- config/software/one-integration-vmcatcher.rb
+++ config/software/one-integration-vmcatcher.rb
@@ -7,9 +7,7 @@ build do
   mkdir "#{install_dir}/etc"
   mkdir "#{install_dir}/etc/cron.d"
   mkdir "#{install_dir}/etc/init.d"
-  mkdir "#{install_dir}/etc/vmcatcher"
   copy "#{project.files_path}/bin/one-integration-vmcatcher", "#{install_dir}/bin/one-integration-vmcatcher"
   copy "#{project.files_path}/etc/cron.d/one-integration-vmcatcher", "#{install_dir}/etc/cron.d/one-integration-vmcatcher"
   copy "#{project.files_path}/etc/init.d/one-integration-vmcatcher-cron", "#{install_dir}/etc/init.d/one-integration-vmcatcher-cron"
-  copy "#{project.files_path}/etc/vmcatcher/vmcatcher-env", "#{install_dir}/etc/vmcatcher/vmcatcher-env"
 end
